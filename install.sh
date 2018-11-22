a4e6c0850c4575fc11daf69f1c00f071df75ae37
Spirin Egor <egor@spirin.tech>
2018-11-22 19:40:55 +0300

    move compile to install stage

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "----- Install dependencies -----"
-
+g++ --std=c++17 simple_solution.cpp
--- run.sh
+++ run.sh
@@ -5,5 +5,4 @@ INPUT_GRAPH_FILE=$2
 OUTPUT_FILE=$3
 
 # call your programm here
-g++ --std=c++17 simple_solution.cpp
 ./a.out $1 $2 $3
