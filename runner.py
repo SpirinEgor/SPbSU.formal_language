554a5c75aeba8e9a71df24bc581a92f92475cddf
Spirin Egor <egor@spirin.tech>
2018-11-22 19:45:25 +0300

    remove interface test add comment for sample

--- runner.py
+++ runner.py
@@ -5,7 +5,7 @@ from typing import Generator
 from tests.ITest import ITest
 from tests.SimpleTest import SimpleTest
 
-tests = [ITest(), SimpleTest()]
+tests = [SimpleTest()]
 
 for test in tests:
     print('=== {} ==='.format(test.name))
@@ -14,7 +14,7 @@ for test in tests:
         subprocess.call(['./run.sh', sample.chomsky_file, sample.graph_file, 'answer.txt'])
         finish_time = time.time()
         if sample.check_equal('answer.txt'):
-            print('test #{} done in {}s'.format(id + 1, finish_time - start_time))
+            print('test #{} ({}): done in {}s'.format(id + 1, sample.comment, finish_time - start_time))
         else:
-            print('test #{}: uncorrect'.format(id + 1))
+            print('test #{} ({}): uncorrect'.format(id + 1, sample.comment))
             break
