4accf69aec908574c0ce9dacdc2b55748cc83d1e
KutuevVladimir <VladimirKutuev@gmail.com>
2018-12-23 03:37:43 +0300

    fixed install.sh

--- install.sh
+++ install.sh
@@ -8,7 +8,6 @@ echo "----- CMake -----"
 wget https://github.com/Kitware/CMake/releases/download/v3.13.2/cmake-3.13.2-Linux-x86_64.tar.gz
 tar xzf cmake-3.13.2-Linux-x86_64.tar.gz
 rm cmake-3.13.2-Linux-x86_64.tar.gz
-# cd cmake-3.13.2-Linux-x86_64
 
 echo "----- M4ri -----"
 git clone https://vkutuev@bitbucket.org/vkutuev/m4ri.git
@@ -17,7 +16,7 @@ autoreconf --install
 autoreconf --install
 ./configure
 sudo make install
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
+echo 'export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/usr/local/lib"' >> $BASH_ENV
 cd ..
 
 echo "----- Make program -----"
