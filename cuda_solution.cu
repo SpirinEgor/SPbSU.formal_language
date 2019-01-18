8e472efb6c542409238aff09abff1916edfd1a56
Spirin Egor <egor@spirin.tech>
2019-01-18 14:48:49 +0300

    move main logic to cuda_solution

--- cuda_solution.cu
+++ cuda_solution.cu
@@ -1,15 +1,35 @@
-// #include <stdio.h>
-// #include <stdlib.h>
-// #include <time.h>
-// #include <sys/time.h>
-#include <iostream>
-#include <ctime>
-
+#include <fstream>
+#include <vector>
+#include <unordered_map>
 
 // 32x32 Threads in a block.
 #define NTHREADS_X 32
 #define NTHREADS_Y 32
 #define THREADS_PER_BLOCK NTHREADS_X * NTHREADS_Y
+
+using std::string;
+using std::vector;
+using std::unordered_map;
+
+const int MAXN = static_cast<int>(1e3);
+
+struct rule {
+    string from_, to1_, to2_;
+
+    rule(string from, string to1, string to2) : from_(from), to1_(to1), to2_(to2) {}
+};
+
+char g[MAXN][MAXN];
+vector<rule> prod;
+
+int ver_num;
+bool tmp_mat[30][MAXN][MAXN];
+bool mat[30][MAXN * MAXN];
+
+unordered_map<string, int> reverse_prod;
+int cnt = 0;
+unordered_map<string, int> bij;
+
 
 /* 
 * A macro used for error checking in CUDA function calls
@@ -25,9 +45,10 @@ inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort=t
    }
 }
 
-__global__ void matrix_mul(int *a, int *b, int *c, int a_ncolumns, int c_nlines,
+__global__ void matrix_mul(bool *a, bool *b, bool *c, int a_ncolumns, int c_nlines,
         int c_ncolumns, int nBlocks) {
-    int i, z, cur_value = 0;
+    int i, z;
+    bool cur_value = 0;
 
     // number of multiplications
     int nMultiplications = a_ncolumns;
@@ -38,8 +59,8 @@ __global__ void matrix_mul(int *a, int *b, int *c, int a_ncolumns, int c_nlines,
     int column = blockIdx.x * blockDim.x + threadIdx.x;
     int line =  blockIdx.y * blockDim.y + threadIdx.y;
 
-    __shared__ int s_a[NTHREADS_Y][NTHREADS_X];
-    __shared__ int s_b[NTHREADS_Y][NTHREADS_X];
+    __shared__ bool s_a[NTHREADS_Y][NTHREADS_X];
+    __shared__ bool s_b[NTHREADS_Y][NTHREADS_X];
 
     int a_tLine, a_tColumn, b_tLine, b_tColumn;
 
@@ -61,7 +82,7 @@ __global__ void matrix_mul(int *a, int *b, int *c, int a_ncolumns, int c_nlines,
 
         __syncthreads();
 
-	// Checkin position in Matrix C
+	    // Checkin position in Matrix C
         if (column < c_ncolumns && line < c_nlines) {
             if (nMultiplications < NTHREADS_Y) {
                 multiplicationsInBlock = nMultiplications;
@@ -78,132 +99,124 @@ __global__ void matrix_mul(int *a, int *b, int *c, int a_ncolumns, int c_nlines,
     }
 
     // Set value to Matrix C
-    bool has_changed = false;
     if (column < c_ncolumns && line < c_nlines) {
-	has_changed |= (cur_value != c[line * c_ncolumns + column]);
         c[line * c_ncolumns + column] |= cur_value;
     }
-    // return has_changed;
 }
 
-int main()
-{
-    int *a, *b, *c;
-    int *d_a, *d_b, *d_c;
-    int a_nlines, a_ncolumns;
-    int b_nlines, b_ncolumns;
-    int c_nlines, c_ncolumns;
-
-    size_t a_size, b_size, c_size;
-    int i, j;
+bool mult(int res, int a1, int a2) {  // res += a1 * a2
+    bool *d_a, *d_b, *d_c;
 
     cudaEvent_t start, stop;
     gpuErrchk( cudaEventCreate(&start) );
     gpuErrchk( cudaEventCreate(&stop) );
 
-//    scanf("%d", &a_nlines);
-//    scanf("%d", &a_ncolumns);
-//    scanf("%d", &b_nlines);
-//    scanf("%d", &b_ncolumns);
-    std::cin >> a_nlines >> a_ncolumns >> b_nlines >> b_ncolumns;
+    int size = ver_num * ver_num * sizeof(bool);
+    gpuErrchk( cudaMalloc((void **) &d_a, size) );
+    gpuErrchk( cudaMalloc((void **) &d_b, size) );
+    gpuErrchk( cudaMalloc((void **) &d_c, size) );
 
-    c_nlines = a_nlines;
-    c_ncolumns = b_ncolumns;
+    bool* c = (bool*)malloc(size);
+    memset(c, 0, size);
 
-#ifdef __DEBUG
-    printf("a_nlines: %d\na_ncolumns: %d\nb_nlines: %d\nb_ncolumns: %d\nc_nlines: %d\nc_ncolumns: %d\n", a_nlines, a_ncolumns, b_nlines, b_ncolumns, c_nlines, c_ncolumns);
-#endif
-
-    if ( a_ncolumns != b_nlines ) {
-	std::cout << "Number of columns in Matrix A should be equals to number of lines in Matrix B\n";
-        return EXIT_FAILURE;
-    }
-
-    a_size = a_nlines * a_ncolumns * sizeof(int);
-    b_size = b_nlines * b_ncolumns * sizeof(int);
-    c_size = c_nlines * c_ncolumns * sizeof(int);
-
-    gpuErrchk( cudaMalloc((void **) &d_a, a_size) );
-    gpuErrchk( cudaMalloc((void **) &d_b, b_size) );
-    gpuErrchk( cudaMalloc((void **) &d_c, c_size) );
-
-    a = (int *)malloc(a_size);
-    b = (int *)malloc(b_size);
-    c = (int *)malloc(c_size);
-
-    memset(c, 0, c_nlines*c_ncolumns*sizeof(int));
-
-    for (i = 0; i < a_nlines; i++)
-    {
-        for (j = 0; j < a_ncolumns; j++)
-        {
-            scanf("%d", &a[i * a_ncolumns + j]);
-        }
-    }
-
-    for (i = 0; i < b_nlines; i++)
-    {
-        for (j = 0; j < b_ncolumns; j++)
-        {
-            scanf("%d", &b[i * b_ncolumns + j]);
-        }
-    }
-
-    gpuErrchk( cudaMemcpy(d_a, a, a_size, cudaMemcpyHostToDevice) );
-    gpuErrchk( cudaMemcpy(d_b, b, b_size, cudaMemcpyHostToDevice) );
+    gpuErrchk( cudaMemcpy(d_a, mat[a1], size, cudaMemcpyHostToDevice) );
+    gpuErrchk( cudaMemcpy(d_b, mat[a2], size, cudaMemcpyHostToDevice) );
 
     dim3 tbloco = dim3(
-                    (int) std::ceil( (double) c_ncolumns / NTHREADS_X ),
-                    (int) std::ceil( (double) c_nlines / NTHREADS_Y ),
-                    1
-                );
+                    (int) std::ceil( (double) ver_num / NTHREADS_X ),
+                    (int) std::ceil( (double) ver_num / NTHREADS_Y ),
+                    1);
 
-    dim3 tthreads = dim3(
-                        NTHREADS_X,
-                        NTHREADS_Y,
-                        1
-                    );
-
-#ifdef __DEBUG
-    printf("tbloco.x: %d tbloco.y: %d tbloco.z: %d\n", tbloco.x, tbloco.y, tbloco.z);
-    printf("tthreads.x: %d tthreads.y: %d\n", tthreads.x, tthreads.y);
-#endif
+    dim3 tthreads = dim3(NTHREADS_X, NTHREADS_Y, 1);
 
     cudaEventRecord(start);
-
     // kernel call
-    matrix_mul<<<tbloco,tthreads>>>(d_a, d_b, d_c, a_ncolumns, c_nlines,
-        c_ncolumns, (int) std::ceil( (double) a_ncolumns / NTHREADS_X));
+    matrix_mul<<<tbloco,tthreads>>>(d_a, d_b, d_c, ver_num, ver_num,
+        ver_num, (int) std::ceil( (double) ver_num / NTHREADS_X));
 
     gpuErrchk( cudaPeekAtLastError() );
     gpuErrchk( cudaEventRecord(stop) );
-    gpuErrchk( cudaMemcpy(c, d_c, c_size, cudaMemcpyDeviceToHost) );
+    gpuErrchk( cudaMemcpy(c, d_c, size, cudaMemcpyDeviceToHost) );
     gpuErrchk( cudaEventSynchronize(stop) );
 
-#ifndef __NO_OUTPUT
-    // print Matrix
-    for (i = 0; i < c_nlines; i++)
-    {
-        for (j = 0; j < c_ncolumns; j++)
-        {
-            printf("%d ", c[i * c_ncolumns + j]);
-        }
-        printf("\n");
+    bool has_changed = false;
+    for (int i = 0; i < ver_num * ver_num; ++i) {
+        has_changed |= (c[i] != mat[res][i]);
+        mat[res][i] |= c[i];
     }
-    printf("\n");
-#endif
+    free(c);
+    cudaFree(d_a); cudaFree(d_b); cudaFree(d_c);
+    return has_changed;
+}
 
-#ifdef __TIME
-    float milliseconds = 0;
-    gpuErrchk( cudaEventElapsedTime(&milliseconds, start, stop) );
-    printf("%.5f\n", milliseconds);
-#endif
+int main(int argc, char* argv[]) {
+    auto nfh_stream = std::ifstream(argv[1], std::ifstream::in);
+    string from, to1, to2;
+    while (nfh_stream >> from >> to1) {
+        if (to1[0] >= 'A' && to1[0] <= 'Z') {
+            nfh_stream >> to2;
+            prod.emplace_back(from, to1, to2);
+            if (!bij.count(to1)) {
+                bij[to1] = cnt++;
+            }
+            if (!bij.count(to2)) {
+                bij[to2] = cnt++;
+            }
+        } else {
+            if (!bij.count(from)) {
+                bij[from] = cnt++;
+            }
+            reverse_prod[to1] = bij[from];
+        }
+    }
+    nfh_stream.close();
+    auto graph_stream = std::ifstream(argv[2], std::ifstream::in);
+    int a, b;
+    string c;
+    int num_vertices = 0;
+    while (graph_stream >> a >> c >> b) {
+        tmp_mat[reverse_prod[c]][a - 1][b - 1] = 1;
+        num_vertices = max(num_vertices, max(a - 1, b - 1));
+    }
+    graph_stream.close();
+    ++num_vertices;
+    ver_num = num_vertices;
+    for (auto & term : reverse_prod) {
+        for (int i = 0; i < ver_num; ++i) {
+            for (int j = 0; j < ver_num; ++j) {
+                mat[term.second][i * ver_num + j] = tmp_mat[term.second][i][j];
+            }
+        }
+    }
+    // int iter_num = 1;
 
-    free(a); free(b); free(c);
-
-    cudaFree(d_a);
-    cudaFree(d_b);
-    cudaFree(d_c);
-
+    // auto time_start = chrono::steady_clock::now();
+    while (true) {
+        bool has_changed = false;
+        for (auto & cur_prod : prod) {
+            has_changed |= mult(bij[cur_prod.from_], bij[cur_prod.to1_], bij[cur_prod.to2_]);
+        }
+        if (!has_changed) {
+            break;
+        }
+        // ++iter_num;
+    }
+    // auto time_finish = chrono::steady_clock::now();
+    auto out_stream = std::ofstream(argv[3], std::ifstream::out);
+    for (auto& s : bij) {
+        if (s.first[0] <= 'A' || s.first[0] >= 'Z') {
+            continue;
+        }
+        out_stream << s.first << " ";
+        for (int i = 0; i < num_vertices; ++i) {
+            for (int j = 0; j < num_vertices; ++j) {
+                if (mat[s.second][i * ver_num + j]) {
+                    out_stream << i + 1 << " " << j + 1 << " ";
+                }
+            }
+        }
+        out_stream << '\n';
+    }
+    out_stream.close();
     return 0;
 }
--- run.sh
+++ run.sh
@@ -4,5 +4,5 @@ INPUT_CHOMSKY_FILE=$1
 INPUT_GRAPH_FILE=$2
 OUTPUT_FILE=$3
 
-# call your programm here
+# call your program here
 ./a.out $1 $2 $3
